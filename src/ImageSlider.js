3a9188fa6b1e4d009466ff9434189f3db305aada
Lindsay Mecher <lindsaymecher@gmail.com>
2020-03-10 13:10:15 -0500

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,29 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+class ImageSlider extends Component{
+  constructor(){
+    super()
+    this.state={
+      currentSlideIndex: 0
+    }
+  }
+
+  renderText = () => {
+    // if (this === this.state.currentSlideIndex) {
+    return `I am on slide ${this.state.currentSlideIndex}`
+    // }
+  }
+
+  render(){
+    return(
+      <h3>{this.renderText()}</h3>
+    )
+  }
+}
+
+export default ImageSlider;
+
+
+// 2.  It should only render out the text `'I am on slide <CURRENT_SLIDE>'`, where
+//     `<CURRENT_SLIDE>` is the value of `this.state.currentSlideIndex`.
